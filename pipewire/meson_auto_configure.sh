9b30bb101bc38262b410f150902c763ce999ac33
souravdas142 <souravdas142@gmail.com>
2021-05-08 14:54:16 +0530

    Adding some helper script and lists

---
+++ pipewire/meson_auto_configure.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+meson configure build  --buildtype=plain \
+	--wrap-mode=nodownload --buildtype=plain \
+	--prefix=/usr --sysconfdir=/etc --localstatedir=/var \
+    --libdir=lib/x86_64-linux-gnu \
+	-D audiotestsrc=enabled \
+	-D auto_features=enabled \
+	-D b_lto=true \
+	-D b_pie=true \
+	-D libcamera=disabled \
+	-D docs=enabled \
+	-D examples=enabled \
+	-D installed_tests=enabled \
+	-D man=enabled \
+	-D test=enabled \
+	-D videotestsrc=enabled \
+	-D volume=enabled 
+
+
